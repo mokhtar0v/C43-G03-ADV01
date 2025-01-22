6a292c8fb7d04d1f9641d73455444ea9f0c77316
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-23 01:44:06 +0200

    Question02

--- Session Assignment/Program.cs
+++ Session Assignment/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
         public static void BubbleSort(int[] arr)
         {
             int n = arr.Length;
@@ -10,26 +16,47 @@
             for (int i = 0; i < n - 1; i++)
             {
                 flag = false;
-
-                // The last i elements are already sorted, so reduce the range
                 for (int j = 0; j < n - i - 1; j++)
                 {
                     if (arr[j] > arr[j + 1])
                     {
-                        // Swap adjacent elements if they are in the wrong order
                         Swap(ref arr[j], ref arr[j + 1]);
                         flag = true;  // Mark that a swap occurred
                     }
                 }
-
-                // If no elements were swapped, the array is already sorted
                 if (!flag)
                     break;
             }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Qeustion01
+            //int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+            //Console.WriteLine("Before Sort:");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //BubbleSort(arr);
+
+            //Console.WriteLine("After Sort:");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //} 
+            #endregion
+
+            #region Question02
+            doubleRange<int> intRange = new Range<int>(10, 50);
+            Console.WriteLine(intRange.IsInRange(30)); 
+            Console.WriteLine(intRange.Length()); 
+
+            Range <double> doubleRange = new Range<double>(5.5, 10.2);
+            Console.WriteLine(doubleRange.IsInRange(7.8));
+            Console.WriteLine(doubleRange.Length()); 
+            #endregion
         }
     }
 }
